3c8892bd9d58b1bdb728eb91f7358ef849cba61b
tommy <afdg@protonmail.com>
2021-09-14 21:40:56 +1200

    Unsure about drawing order to delete and replace piece

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 # Other Variables for use in the program
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 1000
 SPEED = 5
 
 # Create a black screen
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-DISPLAYSURF.fill(BLACK)
+
 pygame.display.set_caption("Game")
 
 # CONSTANTS
@@ -45,20 +45,20 @@ class Piece(pygame.sprite.Sprite):
     def __init__(self):
         self.type = random.choice(types)
         self.rotation = 0
-        self.x = 4
-        self.y = 20 # adapt for different sized boards
+        self.x = 10
+        self.y = 10 # adapt for different sized boards
 
     def rotate(self):
-        rotation = (rotation+1) % len(type)
+        self.rotation = (self.rotation+1) % len(self.type)
 
 # Draw piece
-def draw(x,y,piece):
+def draw(piece, x, y, rotation):
     relX = 0
     relY = 0
     for i in piece.type[piece.rotation]:
+
         if i == "1":
-            pygame.draw.rect(DISPLAYSURF, RED, ((x+relX)*BLOCKSIZE,(y+relY)*BLOCKSIZE,BLOCKSIZE,BLOCKSIZE))
-        print(piece.type[piece.rotation])
+            pygame.draw.rect(DISPLAYSURF, RED, ((piece.x+relX)*BLOCKSIZE,(piece.y+relY)*BLOCKSIZE,BLOCKSIZE,BLOCKSIZE))
         relX+=1
         if relX == 4:
             relX = 0
@@ -68,10 +68,28 @@ def draw(x,y,piece):
 
 nextPiece = Piece()
 # Game Loop
+
 while True:
 
 
-    draw(5,10,nextPiece)
+    # Keypressing
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            break
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+                break
+            elif event.key == pygame.K_RIGHT:
+                draw(nextPiece, 1, 0, 0)
+            elif event.key == pygame.K_LEFT:
+
+            elif event.key == pygame.K_UP:
+
+
+
+
 
 
     pygame.display.update()

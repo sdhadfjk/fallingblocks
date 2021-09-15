9f37d3dbdd9cdf51a98c07b883ba85363c399343
tommy <afdg@protonmail.com>
2021-09-16 10:19:00 +1200

    Adding to board works, movement maybe, weird error

--- main.py
+++ main.py
@@ -12,11 +12,16 @@ FPS = 60
 FramePerSec = pygame.time.Clock()
 
 # Creating colors
-BLUE = (0, 0, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 255)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (255, 0, 255)
 WHITE = (255, 255, 255)
+
+COLORS = [BLACK,RED,YELLOW,GREEN,CYAN,BLUE,PURPLE,WHITE]
 
 # Other Variables for use in the program
 SCREEN_WIDTH = 500
@@ -30,6 +35,8 @@ pygame.display.set_caption("Game")
 
 # CONSTANTS
 BLOCKSIZE = 20
+BOARD_WIDTH = 10 # Starts at 0, 9 is recommended
+BOARD_HEIGHT = 20 # Stars at 0, 19 is recommended
 
 # pieces layout
 I = ["0010001000100010", "0000111100000000"]
@@ -39,55 +46,55 @@ S = ["0000011000110000", "000100110010"]
 L = ["0000011101000000", "0110001000100000", "0001011100000000", "0010001000110000"]
 J = ["0000011100010000", "0011001000100000", "0000011100010000", "0010001001100000"]
 T = ["0000011100100000", "0010011100100000", "0010011100000000", "0010001100100000"]
-types = [I,O,Z,S,L,J,T]
+types = [0,I,O,Z,S,L,J,T]
+board = []
+
+def generateBoard():
+    for i in range(0,BOARD_HEIGHT):
+        sublist=[]
+        for j in range(0,BOARD_WIDTH):
+           sublist.append(0)
+        board.append(sublist)
 
 class Piece(pygame.sprite.Sprite):
     def __init__(self):
-        self.type = random.choice(types)
+        super().__init__()
+        self.typenum = random.randint(1,7)
         self.rotation = 0
-        self.x = 10
-        self.y = 10 # adapt for different sized boards
+        self.x = 3 # adapt for different sized boards
+        self.y = 0
+        addPosition(self)
 
-    def rotate(self):
-        self.rotation = (self.rotation+1) % len(self.type)
-
-# Draw piece
-def draw(piece, x, y, rotation):
+def addPosition(piece):
     relX = 0
     relY = 0
-    for i in piece.type[piece.rotation]:
-
-        if i == "1":
-            pygame.draw.rect(DISPLAYSURF, RED, ((piece.x+relX)*BLOCKSIZE,(piece.y+relY)*BLOCKSIZE,BLOCKSIZE,BLOCKSIZE))
+    for i in types[piece.typenum][piece.rotation]: # in type string
+        board[piece.y + relY][piece.x + relX] = int(i)
         relX+=1
         if relX == 4:
-            relX = 0
             relY+=1
+            relX=0
         if relY == 4:
             break
+generateBoard()
+currentPiece = Piece()
 
-nextPiece = Piece()
 # Game Loop
 
 while True:
+    # Draw board
+    for i in range(BOARD_HEIGHT):
+        for j in range(BOARD_WIDTH):
+            pygame.draw.rect(DISPLAYSURF, COLORS[board[i][j]], (j * BLOCKSIZE, i * BLOCKSIZE, BLOCKSIZE, BLOCKSIZE))
 
+    key = pygame.key.get_pressed()
 
-    # Keypressing
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            break
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
-                break
-            elif event.key == pygame.K_RIGHT:
-                draw(nextPiece, 1, 0, 0)
+    if key[pygame.K_LEFT]:
+        currentPiece.x -= 1
 
+    if key[pygame.K_RIGHT]:
+        currentPiece.x += 1
 
-
-
-
-
+    addPosition(currentPiece)
     pygame.display.update()
     FramePerSec.tick(FPS)
